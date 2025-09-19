e2770d726d7878d1adb3f5c90c66656bb795fbb6
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-09-19 12:54:16 +0900

    Remove p since printing was integrated in to knights_travails.rb

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative 'lib/knights_travails'
 
 knights_travails = KnightsTravails.new
 
-p knights_travails.knight_moves([3, 3], [1, 1])
-p knights_travails.knight_moves([0, 0], [3, 3])
-p knights_travails.knight_moves([3, 3], [0, 0])
-p knights_travails.knight_moves([0, 0], [7, 7])
+knights_travails.knight_moves([3, 3], [1, 1])
+knights_travails.knight_moves([0, 0], [3, 3])
+knights_travails.knight_moves([3, 3], [0, 0])
+knights_travails.knight_moves([0, 0], [7, 7])
